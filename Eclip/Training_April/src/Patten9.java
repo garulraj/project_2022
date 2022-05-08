92ad83cfa5a03affc8b1b00520a5e5781c7915ee
garulraj <sendtomr.arulraj@gmail.com>
2022-05-08 14:44:27 +0530

    copying

---
+++ Eclip/Training_April/src/Patten9.java
@@ -0,0 +1,20 @@
+
+public class Patten9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int row=5;row>=1;row--){
+			for(int col=1;col<=5;col++) {
+				System.out.print("*");
+			}System.out.println();
+			
+		}
+	}
+
+}
+/*single 17.5-800sq-500-2.25
+double -- 700sq--22/--
+80%            
+//9-5;-pradeep--perumal realestate*/
+
